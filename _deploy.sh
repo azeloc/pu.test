5f9bb80152762ed7e2dfbc8a5740c1f765e9a7fa
azeloc <fe_poliano@yahoo.com.br>
2017-01-10 23:33:12 -0200

    update deploy.sh

--- _deploy.sh
+++ _deploy.sh
@@ -5,7 +5,7 @@ set -e
 [ -z "${GITHUB_PAT}" ] && exit 0
 [ "${TRAVIS_BRANCH}" != "master" ] && exit 0
 
-git config --global user.email "fptcorrea@gmail,com"
+git config --global user.email "fptcorrea@gmail.com"
 git config --global user.name "azeloc"
 
 git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git output
