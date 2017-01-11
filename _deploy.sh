ce1a3b2cd6d1ddf59d48cb4c3afea7189b3ea23b
azeloc <fe_poliano@yahoo.com.br>
2017-01-10 23:25:17 -0200

    teste do deploy

---
+++ _deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+[ -z "${GITHUB_PAT}" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+
+git config --global user.email "fptcorrea@gmail,com"
+git config --global user.name "azeloc"
+
+git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git output
+cd output
+git add --all *
+git commit -m "Update the rep" || true
+git push origin content
