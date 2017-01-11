0e752d2aeaa1233f537993a248fc7ca08b1bb76b
azeloc <fe_poliano@yahoo.com.br>
2017-01-10 23:35:09 -0200

    update no deploy

--- _deploy.sh
+++ _deploy.sh
@@ -9,7 +9,7 @@ git config --global user.email "fptcorrea@gmail.com"
 git config --global user.name "azeloc"
 
 git clone -b content https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git output
-cd content
+cd output/content
 git add --all *
 git commit -m "Update the rep" || true
 git push origin content
